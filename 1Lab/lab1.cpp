fa14c68f3db0d317d6c188905a21644a10ec7039
Knox Crichton <knoxcrichton@gmail.com>
2019-08-27 10:21:22 -0400

    First commit, includes Lab1 work

---
+++ 1Lab/lab1.cpp
@@ -0,0 +1,18 @@
+// Comment
+/*Multi Line Comment*/
+//Dunn - section 2?
+//August 21 2019
+//Lab1, tutorial lab covering basic linux command and c++ output
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+	string n;
+	cout << "What is your name ?";
+	cin >> n;
+	cout << "Hello " << n << endl;
+
+	return 0;
+}
+
